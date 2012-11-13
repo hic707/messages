03e3215de4f8acfec13dc86e088a5f217589523b
hic707 <shic.-_-.tsukasa@nifty.com>
2012-11-13 21:02:32 +0900

    23章済
    23章Rails view specを完了した

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,17 @@
+class MessagesController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  	@message = Message.new(params[:message])
+  	if @message.save
+  	  flash[:notice] = "The message was saved successfully."
+  	  redirect_to :action => "index"
+  	else
+  	  render :action => "new"
+  	end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messages::Application.config.secret_token = 'e4f4e6245b0fc0352d55f67276c689a1d69aae1836458835958e4a345a34367462d16f5402d955fe9de72a13373a2af16e5232c2a078f10cb648f34e85aa1457'
